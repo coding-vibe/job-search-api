88856fc05474abcd9f73572e4aaffbfb5c3ea776
Ihor Sokolov <sihorjs@gmail.com>
2025-06-11 16:09:45 +0300

    add container

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
-export default function initDb() {
-  return mongoose.connect(process.env.DB_URL as string);
+export default async function initDb() {
+  try {
+
+    await mongoose.connect(process.env.DB_URL as string);
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.log(error);
+
+  }
 }
